d53900d7876223408610af3bcbe4e05e65334c0e
Daniel Lundholm <Daniel7805@hotmail.com>
2022-02-25 01:04:20 +0100

    challenge complete

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spamDogDelay = 0.5f;
+    private bool goForDog = true;
+
+    void Start() => InvokeRepeating("GoForDog", 0, spamDogDelay);
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && goForDog)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            goForDog = false;
+        }
+    }
+
+    void GoForDog() => goForDog = true;
+}
